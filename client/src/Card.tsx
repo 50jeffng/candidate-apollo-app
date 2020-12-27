c33ce6491931e496bc5390771baf29b7b976b7a5
50jeffng <50jeffng@gmail.com>
2020-12-27 11:34:38 +0800

    Made Card touch highlightable with rounded corners

--- client/src/Card.tsx
+++ client/src/Card.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, TouchableHighlight} from 'react-native';
 
 const Card = (props: {children: React.ReactNode}) => {
     return (
         <View style={styles.card}>
-            <View style={styles.cardContent}>
-                { props.children }
-            </View>
+            <TouchableHighlight
+                activeOpacity={0.6}
+                underlayColor="#DDDDDD"
+                onPress={()=>{}}
+            >
+                <View style={styles.cardContent}>
+                    { props.children }
+                </View>
+            </TouchableHighlight>
         </View>
     )
 };
 
 const styles = StyleSheet.create({
     card: {
-        borderRadius: 6,
-        elevation: 3,
+        overflow: 'hidden',
+        borderRadius: 15,
+        elevation: 5,
         backgroundColor: '#fff',
         shadowOffset: {width: 1, height: 1},
         shadowColor: '#333',
         shadowOpacity: 0.3,
         shadowRadius: 2,
         marginHorizontal: 4,
-        marginVertical: 6,
+        marginVertical: 10,
     },
     cardContent: {
         marginHorizontal: 18,
