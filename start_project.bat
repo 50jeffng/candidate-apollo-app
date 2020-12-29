052559330a4f95c0af944b3163bf136a2c658112
50jeffng <50jeffng@gmail.com>
2020-12-29 12:55:18 +0800

    Changed windows batch script so that it starts up mongodb server

--- start_project.bat
+++ start_project.bat
@@ -1,6 +1,9 @@
 @ECHO OFF
+REM Run mongodb server, gets first line from mongodb_path.txt, it should be your mongodb directory
+SET /p DBPATH=<mongodb_path.txt
+START cmd.exe /k "mongod --dbpath=%DBPATH%"
 CD .\client
-Rem Changes emulator port to point to localhost port, change '8080' to process.env.SERVER_PORT
+REM Changes emulator port to point to localhost port, change '8080' to process.env.SERVER_PORT
 adb reverse tcp:8080 tcp:8080
 START cmd.exe /k "yarn start"
 START cmd.exe /c "npx react-native run-android"
